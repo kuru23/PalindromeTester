ff85a8ec65ccddeab6cc5cbc727bb13d07ddd3af
Matt Fay <mattfay23@gmail.com>
2024-12-15 17:18:13 -0800

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+// Matt Fay
 function check(){
     //prompt for input
  let inputString = prompt("Is this string a palindrome?")
@@ -16,3 +17,22 @@ function check(){
 }
 
 }
+
+
+function login(){
+    let f = document.getElementById("first").value
+    let l = document.getElementById("last").value
+    var img = document.getElementById('ham');
+    var nameString = `${f} ${l}`
+    if (nameString.length < 21){
+    console.log(nameString)
+    let z = document.getElementById("zip").value
+
+console.log(z)
+console.log(z.length)
+if (z.length == 5){ 
+document.getElementById("Answer").innerHTML = "we're sending you a free hamster"
+img.style.visibility = "visible";
+} else {img.style.visibility = "hidden";} 
+
+}}
